3a228a10407e2f705668ef41d443b7526cdb24f7
CLg003 <clairemjlaing@gmail.com>
2022-04-01 09:46:19 +0100

    Added CSS.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
-  
-  // Only this (commented out) event listener in solution:
-  // const newItemForm = document.querySelector('#new-item-form');
-  // newItemForm.addEventListener('submit', handleNewItemFormSubmit);
-  
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+
   const titleInput = document.querySelector('#title');
   titleInput.addEventListener('input', handleTitleInput);
 
@@ -13,27 +12,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const categorySelect = document.querySelector('#category');
   categorySelect.addEventListener('select', handleCategorySelect);
-
-  const form = document.querySelector('#new-item-form');
-  form.addEventListener('submit', handleFormSubmit);
 })
 
 const handleFormSubmit = function (event) {
   event.preventDefault();
-  
-// SOLUTION:
-  // const readingListItem = createReadingListItem(event.target);
-  // const readingList = document.querySelector('#reading-list');
-  // readingList.appendChild(readingListItem);
-  // event.target.reset();
-
   createListItem(event);
   event.target.reset();
 }
 
-// From here to end one function in solution
 const handleTitleInput = function(event) {
   const title = document.createElement('h3');
+  title.classList.add('author');
   title.textContent = event.target.title.value;
   return title;
 }
@@ -52,13 +41,13 @@ const handleCategorySelect = function(event) {
 
 const createListItem = function(event) {
   const newListItem = document.createElement('li');
+  newListItem.classList.add('reading-list-item');
   newListItem.appendChild(handleTitleInput(event));
   newListItem.appendChild(handleAuthorInput(event));
   newListItem.appendChild(handleCategorySelect(event));
   const list = document.querySelector('ul'); 
   list.appendChild(newListItem);
 }
-// Single function in solution finishes here
 
 
 
