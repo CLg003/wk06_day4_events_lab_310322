4293fa0d5fc055954098bed5b9818301b079b28e
CLg003 <clairemjlaing@gmail.com>
2022-04-01 11:39:40 +0100

    MVP and extensions complete, code refactored to single event listener.

--- js/app.js
+++ js/app.js
@@ -4,50 +4,69 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('#new-item-form');
   form.addEventListener('submit', handleFormSubmit);
 
-  const titleInput = document.querySelector('#title');
-  titleInput.addEventListener('input', handleTitleInput);
-
-  const authorInput = document.querySelector('#author');
-  authorInput.addEventListener('input', handleAuthorInput);
-
-  const categorySelect = document.querySelector('#category');
-  categorySelect.addEventListener('select', handleCategorySelect);
+  const deleteAll = document.querySelector('#delete-all');
+  deleteAll.addEventListener('click', handleDeleteAllClick);
 })
 
 const handleFormSubmit = function (event) {
   event.preventDefault();
-  createListItem(event);
+  createListItem(event.target);
   event.target.reset();
 }
 
-const handleTitleInput = function(event) {
-  const title = document.createElement('h3');
-  title.classList.add('author');
-  title.textContent = event.target.title.value;
-  return title;
-}
-
-const handleAuthorInput = function(event) {
-  const author = document.createElement('h4');
-  author.textContent = event.target.author.value;
-  return author;
-}
-
-const handleCategorySelect = function(event) {
-  const category = document.createElement('p');
-  category.textContent = event.target.category.value;
-  return category;
-}
-
-const createListItem = function(event) {
+const createListItem = function(form) {
   const newListItem = document.createElement('li');
   newListItem.classList.add('reading-list-item');
-  newListItem.appendChild(handleTitleInput(event));
-  newListItem.appendChild(handleAuthorInput(event));
-  newListItem.appendChild(handleCategorySelect(event));
+
+  const title = document.createElement('h3');
+  title.textContent = form.title.value;
+  newListItem.appendChild(title);
+
+  const author = document.createElement('h4');
+  author.textContent = form.author.value;
+  newListItem.appendChild(author);
+
+  const category = document.createElement('p');
+  category.textContent = form.category.value;
+  newListItem.appendChild(category);
+
   const list = document.querySelector('ul'); 
   list.appendChild(newListItem);
 }
 
+const handleDeleteAllClick = function (event) {
+  readingList = document.querySelector('#reading-list');
+  readingList.textContent = '';
+}
 
 
+
+// (Book details event listeners replaced with single form 'submit' event)
+  // const titleInput = document.querySelector('#new-item-form');
+  // titleInput.addEventListener('submit', handleTitleInput);
+
+  // const authorInput = document.querySelector('#new-item-form');
+  // authorInput.addEventListener('submit', handleAuthorInput);
+
+  // const categorySelect = document.querySelector('#new-item-form');
+  // categorySelect.addEventListener('submit', handleCategorySelect);
+
+
+// Original functions for handling book details:
+// const handleTitleInput = function(form) {
+//   const title = document.createElement('h3');
+//   title.textContent = form.title.value;
+//   newListItem.appendChild(title);
+// }
+
+// const handleAuthorInput = function(form) {
+//   const author = document.createElement('h4');
+//   author.textContent = form.author.value;
+//   newListItem.appendChild(author);
+// }
+
+// const handleCategorySelect = function(form) {
+//   const category = document.createElement('p');
+//   category.textContent = form.category.value;
+//   newListItem.appendChild(category);
+// }
